603857b1dbdadb4e87f1323429b90fb7eb1a21c2
GMeyr <gabrielmeyr@gmail.com>
2016-04-04 14:00:35 -0400

    (feat) Update event works, editor modal & overlay close

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,89 @@
 angular.module('EventManager', [])
 
-  .controller('mainCtrl', ['$scope', function($scope){
-    
-  $scope.initModals = function() {
-    $('.modal-trigger').leanModal();
-  };
+  .controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 
-  $scope.focusOnEvent = function(event){
-    $scope.focus = event;
-  };
-
-
-
-
-         VividSeats.eventService.all(
-                      function(arr){
-                        console.log('arr', arr);
-                        $scope.stuff = arr;
-                        console.log('scope.events', $scope.stuff);
-                        $scope.$apply();
-                      }, function(){
-                        console.log('failed to retrieve events');
-                      });
-
-      $scope.updateEvent = VividSeats.eventService.update( focus,
-    function(){
-      console.log('update success');
+  VividSeats.eventService.all(
+    function(arr){
+      console.log('arr', arr);
+      $scope.eventArr = arr;
+      $scope.$apply();
     }, function(){
-      console.log('update failed');
+      console.log('failed to retrieve events');
     });
+    
+    $scope.initModals = function() {
+      $('.modal-trigger').leanModal();
+    };
+
+    $scope.focusOnEvent = function(event){
+      $scope.focus = event;
+    };
+
+    $scope.backupCloseModal = function(){
+      $timeout(function(){
+        angular.element('#editor').closeModal();
+        angular.element('.lean-overlay').remove();
+      }, 100);
+
+    }
+
+    $scope.updateEvent = function(){
+      
+      var newEvent = deepCopy($scope.focus);
+        
+      newEvent.name = angular.element('#nameInput').val();
+      newEvent.date = angular.element('#dateInput').val();
+      newEvent.venue.name = angular.element('#venueInput').val();
+      newEvent.venue.city = angular.element('#cityInput').val();
+      newEvent.venue.state = angular.element('#stateInput').val();
+      console.log('trying to update', newEvent.id);
+      
+
+      VividSeats.eventService.update( newEvent,
+        function(){
+          console.log('update success');
+          //refresh DOM
+          VividSeats.eventService.all(
+            function(arr){
+              $scope.eventArr = arr;
+              $scope.$apply();
+              $scope.backupCloseModal();
+            }, function(){
+              console.log('failed to retrieve events');
+            });
+        }, function(){
+          console.log('update failed');
+        });
+    };
+
+
+  var deepCopy = function (obj) {
+    if (Object.prototype.toString.call(obj) === '[object Array]') {
+        var out = [], i = 0, len = obj.length;
+        for ( ; i < len; i++ ) {
+            out[i] = arguments.callee(obj[i]);
+        }
+        return out;
+    }
+    if (typeof obj === 'object') {
+        var out = {}, i;
+        for ( i in obj ) {
+            out[i] = arguments.callee(obj[i]);
+        }
+        return out;
+    }
+    return obj;
+    };
   }])
+
+
+  // $scope.updateEvent = VividSeats.eventService.update( focus,
+  //   function(){
+  //     console.log('update success');
+  //   }, function(){
+  //     console.log('update failed');
+  //   });
+  // }])
 
   // this custom directive is not my code, it's a tip from StackOverflow
   .directive('repeatDone', function() {
