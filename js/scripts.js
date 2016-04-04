b663fdc3b8f9d32f0f8ccbf85d678564d716c723
GMeyr <gabrielmeyr@gmail.com>
2016-04-04 17:15:13 -0400

    (fix) Add newlines to end of files

--- js/scripts.js
+++ js/scripts.js
@@ -156,7 +156,6 @@ angular.module('EventManager', [])
   };
 
   function separateIntoCities (arr) {
-    console.log('separateIntoCities');
     var citiesObj = {};
     var citiesArr = [['All', arr.length]];
 
@@ -174,51 +173,6 @@ angular.module('EventManager', [])
 
     $scope.cities = citiesArr;
     $scope.$apply();
-    console.log('$scope.cities', citiesArr);
   }
 }]);
 
-/* API Usage examples
-
-var exampleEvent = {
-          "id" : 132,
-          "name" : "Die Fledermaus",
-          "date" : "2014-01-16T14:00:00",
-          "venue" : {
-            "id" : 3244,
-            "name" : "Civic Opera House",
-            "city" : "Chicago",
-            "state" : "IL"
-          }
-        };
-
-VividSeats.eventService.all(
-  function(arr){
-    arr.forEach(
-      function(e){
-        console.log(e.name);
-      }
-    );
-  }, function(){
-    console.log(' "all" method failed');
-  });
-
-VividSeats.eventService.update( exampleEvent, function(){
-  console.log('update success')
-}, function(){
-  console.log('update failed')
-})
-
-VividSeats.eventService.remove( exampleEvent, function(){
-  console.log('update success')
-}, function(){
-  console.log('update failed')
-})
-
-VividSeats.eventService.add( exampleEvent, function(){
-  console.log('add success')
-}, function(){
-  console.log('add failed')
-})
-
-*/
